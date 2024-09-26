38b6af31832291c656ad281f8d172db808727a52
HamzaShaikh17 <hamzashaikhm123@gmail.com>
2024-09-26 19:28:07 +0530

    adding data_types to basics folder

---
+++ 01_Basic/data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    int a=2;
+    std::cout<<"Value of a is "<<a<<" and size of a is "<<sizeof(a)<<std::endl;
+
+    char ch='H';
+    std::cout<<"Value of ch is "<<ch<<" and size of ch is "<<sizeof(ch)<<std::endl;
+
+    bool b1=true;
+    std::cout<<"Value of b1 is "<<b1<<" and size of b1 is "<<sizeof(b1)<<std::endl;
+
+    double d=2.75;
+    std::cout<<"Value of d is "<<d<<" and size of d is "<<sizeof(d)<<std::endl;
+
+
+    return 0;
+}
