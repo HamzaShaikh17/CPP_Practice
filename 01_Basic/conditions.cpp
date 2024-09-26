70130212ec934d30dc782eefc3d66bc736bf2aa6
HamzaShaikh17 <hamzashaikhm123@gmail.com>
2024-09-26 19:41:14 +0530

    conditions

---
+++ 01_Basic/conditions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int a,b;
+    //take input from user and store in variables a and b
+    std::cin>>a>>b;
+    if(a>b){
+        std::cout<<"a is greater"<<std::endl;
+    }
+    else{
+        std::cout<<"b is greater"<<std::endl;
+
+    }
+}
